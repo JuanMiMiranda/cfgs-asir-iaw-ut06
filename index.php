5a45d49e9889883d910e4a27f4fbb6a7e8518289
JuanMi <juanmiranda_90@hotmail.com>
2022-02-21 21:31:24 +0100

    initial comit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UT06</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<main>
+    <div class="container py-4">
+        <header class="pb-3 mb-4 border-bottom">
+           <center> <span class="fs-3">CFGS ASIR-SP Implantación de Aplicaciones Web</span> </center>
+            <br>
+            <center><span class="fs-4"> UT06 Acceso a bases de datos desde lenguajes de script de servidor </span> </center>
+        </header>
+
+    <!-- Content-->
+    <div class="container">
+        <div class="row justify-content-center">
+           
+<div class="row">
+    <div class="col-4">
+    <div class="list-group" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action" id="list-home-list" data-toggle="list" href="sesiones/sesion01" role="tab" aria-controls="home">Sesion 01</a>
+      <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="sesiones/sesion02" role="tab" aria-controls="profile">Sesion 02</a>
+      <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="sesiones/sesion03" role="tab" aria-controls="messages">Sesion 03</a>
+    </div>
+  </div>
+</div>
+    </div>
+</main>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
+
+
+
---
+++ sesiones/sesion01/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UT06 - Sesion01</title>
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+</head>
+<body>
+<main>
+    <div class="container py-4">
+        <header class="pb-3 mb-4 border-bottom">
+           <center> <span class="fs-3">CFGS ASIR-SP Implantación de Aplicaciones Web</span> </center>
+            <br>
+            <center><span class="fs-4"> UT06 Acceso a bases de datos desde lenguajes de script de servidor </span> </center>
+            <br>
+            <center><span class="fs-4"> SESIÓN 01 </span> </center>
+        </header>
+
+    <!-- Content-->
+    <div class="container">
+        Contenido Sesión 01
+    </div>
+    </div>
+</main>
+<script src="../../js/bootstrap.js"></script>
+</body>
+</html>
